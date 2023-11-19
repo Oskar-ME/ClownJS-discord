947479c119a6d4418166909bdff112417b5626e0
Oskar <oskarmedziak2@gmail.com>
2023-11-19 16:15:08 +0100

    Create clown.js
    -

---
+++ Clown/Clown.js
@@ -0,0 +1,8 @@
+const Eris = require("eris");
+const bot = new Eris("token");
+bot.on("messageCreate", (msg) => {
+    if(msg.author.id === "687047322664042550"){
+        msg.addReaction(":clown:");
+    }
+});
+bot.connect();
